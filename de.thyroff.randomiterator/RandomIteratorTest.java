d5810321e9ff6f403901d6d6415c72bba8a49fcc
joh <44289588+JThyroff@users.noreply.github.com>
2020-10-17 02:59:31 +0200

    Add Tests

---
+++ de.thyroff.randomiterator/RandomIteratorTest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomIteratorTest {
+
+    @org.junit.jupiter.api.Test
+    void hasNext() {
+        RandomIterator<Integer> it = new RandomIterator<>(Arrays.asList(1));
+
+        assertTrue (it.hasNext());
+
+        it = new RandomIterator<>(new LinkedList<>());
+
+        assertFalse(it.hasNext());
+
+        RandomIterator<Integer> finalIt = it;
+        assertThrows(NoSuchElementException.class, finalIt::next);
+    }
+
+    @org.junit.jupiter.api.Test
+    void next() {
+        RandomIterator<Integer> it = new RandomIterator<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        it.forEachRemaining(System.out::println);
+    }
+}
